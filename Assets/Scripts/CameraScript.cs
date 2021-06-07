de78969f4610e951c76e0a5ae404a071ac9ab81f
denoboi <deniz.kaymakci@hotmail.com>
2021-06-07 21:27:51 +0300

    Sumo 1

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float rotateSpeed = 10;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //y ekseninde cevirecegimiz icin up kullaniyoruz
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed * horizontalInput);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody playerRb;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float verticalMove = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * verticalMove * speed);
+    }
+}
