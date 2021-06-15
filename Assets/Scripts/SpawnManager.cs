d82dfa8d5df5a48ffd3848605cf24fe578dbf435
denoboi <deniz.kaymakci@hotmail.com>
2021-06-15 16:45:27 +0300

    Sumo son

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-
+    public int enemyCount;
+    public int spawnNumber = 1;
     public GameObject enemyPrefab;
-    
+    public GameObject powerupPrefab;
+
     void Start()
     {
-       Instantiate(enemyPrefab, EnemySpawnPos(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(spawnNumber); // baslangicta 1 tane gelsin
+        Instantiate(powerupPrefab, GenerateSpawnPos(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            spawnNumber++; //enemy yok oldukca her defasinda artir
+            SpawnEnemyWave(spawnNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPos(), powerupPrefab.transform.rotation);
+        }
     }
-    private Vector3 EnemySpawnPos()
+    private Vector3 GenerateSpawnPos()
     {
         float spawnPosX = Random.Range(-10, 10);
         float spawnPosZ = Random.Range(-10, 10);
@@ -25,4 +34,12 @@ public class SpawnManager : MonoBehaviour
 
         return spawnPos; //return kullanmaliyiz void olmayan metodlarda.
     }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+    }
+
 }
